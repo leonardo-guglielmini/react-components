b653fbffa029c80a9be7c81765580c798791c0e8
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-11-27 15:35:30 +0100

    ADD components | header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import './App.css';
+import Header  from './components/header/header';
 
 function App() {
 
   return (
-    <>
-    </>
+    <body className='body'>
+      <Header/>
+    </body>
   )
 }
 
